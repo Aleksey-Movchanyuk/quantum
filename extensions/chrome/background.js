b14a83f38f3a741c19592ae2378bcb92b9b0b8c4
Aleksey Movchanyuk <aleksey.movchanyuk@gmail.com>
2017-02-07 10:06:41 +0100

    Inint version of extension

---
+++ extensions/chrome/background.js
@@ -0,0 +1,87 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+// A generic onclick callback function.
+function genericOnClick(info, tab) {
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+
+  chrome.tabs.create({
+      url: chrome.extension.getURL('popup.html'),
+      active: false
+    }, function(tab) {
+      // After the tab has been created, open a window to inject the tab
+      chrome.windows.create({
+          tabId: tab.id,
+          type: 'popup',
+          focused: true
+      });
+  });
+
+
+}
+
+// A generic onclick callback function.
+function radioOnClick(info, tab) {
+  console.log("radio item " + info.menuItemId +
+              " was clicked (previous checked state was "  +
+              info.wasChecked + ")");
+}
+
+/* ---------------------------------------------------------
+   Create common menu
+---------------------------------------------------------*/
+var textEditorItem = chrome.contextMenus.create(
+    {"title": "Quantum Text Editor", "onclick": genericOnClick});
+
+var textAnalyzerItem = chrome.contextMenus.create(
+    {"title": "Quantum Text Analyzer", "onclick": genericOnClick});
+
+chrome.contextMenus.create({"type": "separator"});
+
+
+var xItem = chrome.contextMenus.create(
+    {"title": "Quantum Feature X", "onclick": genericOnClick});
+
+var yItem = chrome.contextMenus.create(
+    {"title": "Quantum Feature Y", "onclick": genericOnClick});
+
+/* ---------------------------------------------------------
+   Create context type menu
+---------------------------------------------------------*/
+var contexts = ["editable"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Edit this text with Quantum A.I.";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": genericOnClick});
+  console.log("'" + context + "' item:" + id);
+}
+
+
+// Intentionally create an invalid item, to show off error checking in the
+// create callback.
+console.log("About to try creating an invalid item - an error about " +
+            "item 999 should show up");
+chrome.contextMenus.create({"title": "Oops", "parentId":999}, function() {
+  if (chrome.extension.lastError) {
+    console.log("Got expected error: " + chrome.extension.lastError.message);
+  }
+});
+
+//////////////////////////////////////////////////////////
+// ACTIONS
+
+// Listen for messages from the client side
+
+
+//chrome.app.runtime.onLaunched.addListener(launch);
+chrome.browserAction.onCreated.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'common/js/inject.js'
+	});
+});
